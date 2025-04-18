9d8b4ab5c7879c70f3ab2a9ad38f0451aac0168c
Rakotomalala Rajo <rakotomalalarajonyaina@gmail.com>
2025-04-19 00:22:27 +0300

    add pack zen page

---
+++ GenieDigital/wwwroot/js/scroll.js
@@ -0,0 +1,15 @@
+﻿window.initScrollToTop = () => {
+    const btn = document.querySelector('.scroll-to-top');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            btn.classList.add('show');
+        } else {
+            btn.classList.remove('show');
+        }
+    });
+};
+
+window.scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
