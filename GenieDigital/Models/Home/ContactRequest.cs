2e38e49d85336f8887feb8fbb6aeacd361114dfc
Rakotomalala Rajo <rakotomalalarajonyaina@gmail.com>
2025-04-22 08:16:17 +0300

    make contact form send email

---
+++ GenieDigital/Models/Home/ContactRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace GenieDigital.Models.Home;
+
+public class ContactRequest
+{
+    public required string Email { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string Message { get; set; }
+
+    public string? Object { get; set; }
+}
